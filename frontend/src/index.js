34668eb6be82df49e3d263c4f1311499479c22ad
BATS SÉBASTIEN <sebastien.bats@gmail.com>
2026-07-31 15:32:29 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-console.log('🔍 Vérification de l\'environnement...');
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
